b0f4007f5919c6fc1d099d65462f71ccfc15c4d2
胡红 <>
2022-05-22 14:45:28 +0800

    remove old src code

--- blog/src/org/rule.test.js
+++
@@ -1,6 +0,0 @@
-import Rule from "./rule";
-
-test("test", ()=> {
-    console.log(Rule.HeadlineRe());
-    expect("* BLOG hello".match(Rule.HeadlineRe())).toBe(["* BLOG hello", "*", "BLOG", undefined, "hello", undefined]);
-});
